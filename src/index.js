edb809535bd30ab9cd096c6b6f56fe0931c53117
jallusparta <aleksi.a.partanen@aalto.fi>
2018-11-12 14:31:48 +0200

    Ratkaisu tehtävään 1.2 saakka

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Otsikko = (props) => (
+  <div>
+    <h1>{props.name}</h1>
+  </div>
+)
+
+const Osa = (props) => (
+  <div>
+    <p>{props.osa} {props.tehtavia}</p>
+  </div>
+)
+
+const Sisalto = (props) => (
+  <div>
+    <Osa osa = {props.osa1} tehtavia = {props.tehtavia1}/>
+    <Osa osa = {props.osa2} tehtavia = {props.tehtavia2}/>
+    <Osa osa = {props.osa3} tehtavia = {props.tehtavia3}/>
+  </div>
+)
+
+const Yhteensa = (props) => (
+  <div>
+    <p>yhteensä {props.maara} tehtävää</p>
+  </div>
+)
+
+const App = () => {
+  const kurssi = 'Half Stack -sovelluskehitys'
+  const osa1 = 'Reactin perusteet'
+  const tehtavia1 = 10
+  const osa2 = 'Tiedonvälitys propseilla'
+  const tehtavia2 = 7
+  const osa3 = 'Komponenttien tila'
+  const tehtavia3 = 14
+
+  return (
+    <div>
+      <Otsikko name = {kurssi} />
+      <Sisalto osa1 = {osa1} tehtavia1 = {tehtavia1} osa2 = {osa2} tehtavia2 = {tehtavia2} osa3 = {osa3} tehtavia3 = {tehtavia3} />
+      <Yhteensa maara = {tehtavia1 + tehtavia2 + tehtavia3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
